41ac0e8fb70fa8d8c1f612910ad8076cab10ff8d
latronixo <pamerra@mail.ru>
2025-03-02 17:59:26 +0300

    Initial Commit

---
+++ OnlineStore-2nd-Challenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OnlineStore-2nd-Challenge
+//
+//  Created by Валентин Картошкин on 02.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
