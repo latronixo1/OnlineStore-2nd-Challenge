0b02fe266aa61f6fbabe4ac44b0942606cbed0ef
Бойтик <e.boytik@yandex.ru>
2025-03-04 18:38:50 +0200

    Создал нетворк сервис. Создал хоум вью и контролле

---
+++ OnlineStore-2nd-Challenge/Components/Views/HomeCategoryCell.swift
@@ -0,0 +1,28 @@
+//
+//  HomeCategoryCell.swift
+//  OnlineStore-2nd-Challenge
+//
+//  Created by Евгений on 04.03.2025.
+//
+
+import Foundation
+
+import UIKit
+
+class nameView: UIView {
+    init() {
+        super.init(frame: .zero)
+        setView()
+        setConstraint()
+    }
+    
+    func setView(){
+        
+    }
+    func setConstraint(){
+        
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
--- OnlineStore-2nd-Challenge/Support/SceneDelegate.swift
+++ OnlineStore-2nd-Challenge/Support/SceneDelegate.swift
@@ -14,9 +14,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-//        let auth = UserDefaults.standard.bool(forKey: UserDefaultsStorageKeys.authIsTrue.label) //Добавить реализацию что был пропуск экрана, если пользователь авторизирован
+        let auth = UserDefaults.standard.bool(forKey: UserDefaultsStorageKeys.authIsTrue.label)
         window = UIWindow(windowScene: windowScene)
-        window?.rootViewController = UINavigationController(rootViewController: StartViewController())
+        window?.rootViewController = UINavigationController(rootViewController: auth ? StartViewController() : HomeViewController())
         window?.makeKeyAndVisible()
     }
 
