6379824551d39013d82ec94c2936a0e6abe66bf8
Бойтик <e.boytik@yandex.ru>
2025-03-06 11:31:49 +0200

    Теперь доступна работа с категориями

--- OnlineStore-2nd-Challenge/Components/Views/HeaderHomeCollection.swift
+++ OnlineStore-2nd-Challenge/Components/Views/HeaderHomeCollection.swift
@@ -6,23 +6,22 @@
 //
 
 import UIKit
-
-class HeaderHomeCollection: UICollectionReusableView {
+class HeaderHomeCollectionView: UICollectionReusableView {
     static let reuseIdentifier = "HeaderHomeCollection"
-    lazy var labelForPopularCollectionView: UILabel = {
+    lazy var labelForHomeCollectionView: UILabel = {
         let label = UILabel()
-        label.text = "Popular"
+        label.text = "Category"
         label.textColor = .black
         return label
     }()
-    lazy var buttonForPopularCollectionView: UIButton = {
+    lazy var buttonForHomeCollectionView: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "chevron.compact.down"), for: .normal)
         button.tintColor = .black
         return button
     }()
-    lazy var stackForPopularCollectionView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [labelForPopularCollectionView, buttonForPopularCollectionView])
+    lazy var stackForHomeCollectionView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [labelForHomeCollectionView, buttonForHomeCollectionView])
         stackView.axis = .horizontal
         stackView.distribution = .fillProportionally
         stackView.spacing = 8
@@ -30,29 +29,24 @@ class HeaderHomeCollection: UICollectionReusableView {
     }()
     override init(frame: CGRect) {
         super.init(frame: frame)
-        buttonForPopularCollectionView.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
         setViews()
         setConstraints()
         }
     
     func setViews() {
-        addSubview(stackForPopularCollectionView)
-        stackForPopularCollectionView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(stackForHomeCollectionView)
+        stackForHomeCollectionView.translatesAutoresizingMaskIntoConstraints = false
     }
     func setConstraints(){
         NSLayoutConstraint.activate([
-            stackForPopularCollectionView.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
-            stackForPopularCollectionView.rightAnchor.constraint(equalTo: rightAnchor, constant: -10),
-            stackForPopularCollectionView.topAnchor.constraint(equalTo: topAnchor),
-            stackForPopularCollectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
+            stackForHomeCollectionView.leftAnchor.constraint(equalTo: leftAnchor, constant: 10),
+            stackForHomeCollectionView.rightAnchor.constraint(equalTo: rightAnchor, constant: -10),
+            stackForHomeCollectionView.topAnchor.constraint(equalTo: topAnchor),
+            stackForHomeCollectionView.bottomAnchor.constraint(equalTo: bottomAnchor)
             
         ])
     }
     required init?(coder: NSCoder) {
             fatalError("init(coder:) has not been implemented")
         }
-    @objc func buttonTapped() {
-//        let vc = CategoryViewController()
-//        NavigationController.pushViewController(vc, animated: true)
-    }
 }
--- OnlineStore-2nd-Challenge/Components/Views/HomeCategoryCell.swift
+++ OnlineStore-2nd-Challenge/Components/Views/HomeCategoryCell.swift
@@ -5,22 +5,86 @@
 //  Created by Евгений on 04.03.2025.
 //
 
-import Foundation
-
 import UIKit
 
-class nameView: UIView {
-    init() {
-        super.init(frame: .zero)
+class HomeCategoryCell: UICollectionViewCell {
+    static let reuseIdentifier = "HomeCategoryCell"
+    let firstImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    let secondImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    let thirdImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    let fourthImageView: UIImageView = {
+        let imageView = UIImageView()
+        return imageView
+    }()
+    let labelOfCategory: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17, weight: .bold)
+        return label
+    }()
+    let quantityOfProducts: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 17)
+        return label
+    }()
+    let stackOfCatefory: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fillEqually
+        stackView.spacing = 8
+        return stackView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
         setView()
         setConstraint()
-    }
-    
+        }
     func setView(){
+        addSubview(firstImageView)
+        addSubview(secondImageView)
+        addSubview(thirdImageView)
+        addSubview(fourthImageView)
+        stackOfCatefory.addArrangedSubview(labelOfCategory)
+        stackOfCatefory.addArrangedSubview(quantityOfProducts)
+        addSubview(stackOfCatefory)
+        
+        firstImageView.translatesAutoresizingMaskIntoConstraints = false
+        secondImageView.translatesAutoresizingMaskIntoConstraints = false
+        thirdImageView.translatesAutoresizingMaskIntoConstraints = false
+        fourthImageView.translatesAutoresizingMaskIntoConstraints = false
+        stackOfCatefory.translatesAutoresizingMaskIntoConstraints = false
+        
         
     }
     func setConstraint(){
-        
+        NSLayoutConstraint.activate([
+            firstImageView.topAnchor.constraint(equalTo: topAnchor, constant: 6),
+            firstImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 6),
+            
+            secondImageView.topAnchor.constraint(equalTo: topAnchor, constant: 6),
+            secondImageView.rightAnchor.constraint(equalTo: rightAnchor, constant: -6),
+            secondImageView.leftAnchor.constraint(equalTo: firstImageView.rightAnchor, constant: 6),
+            
+            thirdImageView.topAnchor.constraint(equalTo: firstImageView.bottomAnchor, constant: 6),
+            thirdImageView.leftAnchor.constraint(equalTo: leftAnchor, constant: 6),
+            
+            fourthImageView.topAnchor.constraint(equalTo: secondImageView.bottomAnchor, constant: 6),
+            fourthImageView.leftAnchor.constraint(equalTo: thirdImageView.leftAnchor, constant: 6),
+            fourthImageView.rightAnchor.constraint(equalTo: rightAnchor, constant: 6),
+            
+            stackOfCatefory.topAnchor.constraint(equalTo: fourthImageView.bottomAnchor, constant: 6),
+            stackOfCatefory.leftAnchor.constraint(equalTo: leftAnchor, constant: 6),
+            stackOfCatefory.rightAnchor.constraint(equalTo: rightAnchor, constant: 6)])
     }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
